d2160d2a722bdb7a3e4a68bfd38d553b433ed667
Danna Jim <a.23308051281195@cbtis128.edu.mx>
2024-10-17 15:52:26 -0600

    Add files via upload

---
+++ practica_4_unidad2.py
@@ -0,0 +1,53 @@
+print (" ")
+print("Danna Paola Martinez Jimenez")
+print("no. de control: 1195")
+print("3ro-W")
+print (" ")
+#1-  Crear un diccionario vacío y lo vaya llenado con información
+#sobre una persona (por ejemplo nombre, edad, sexo, teléfono, correo electrónico, etc.) 
+#que se le pida al usuario. Cada vez que se añada un nuevo dato debe imprimirse el contenido del diccionario.
+
+persona = {}  # Diccionario vacío
+
+# Solicitar información al usuario
+nombre = input("Introduce el nombre: ")
+persona['nombre'] = nombre
+print(persona)
+
+edad = input("Introduce la edad: ")
+persona['edad'] = edad
+print(persona)
+
+sexo = input("Introduce el sexo: ")
+persona['sexo'] = sexo
+print(persona)
+
+telefono = input("Introduce el teléfono: ")
+persona['teléfono'] = telefono
+print(persona)
+
+correo = input("Introduce el correo electrónico: ")
+persona['correo'] = correo
+print(persona)
+print (" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
